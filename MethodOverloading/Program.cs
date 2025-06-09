902c5e28f83071aa799a07e749aa1201014cac7a
Prophetica <samin.manshif@gmail.com>
2025-06-09 14:33:27 -0400

    Overloaded the add method

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(1, 2));
+            Console.WriteLine(Add(1.5m, 2.6m));
+            Console.WriteLine(Add(1,0,false));
+        }
+
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        static decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        static string Add(int a, int b, bool c)
+        {
+            if (c)
+            {
+                return a + b == 1 ? "1 dollar" : $"{a + b} dollars";
+            }
+            else
+            {
+                return "";
+            }
         }
     }
 }
